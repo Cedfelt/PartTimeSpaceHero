fbeb8af7e0d964d05ca069b81d0db16aedaf5351
Anders Svensson <Anders.Svensson@labgruppen.com>
2016-04-12 08:13:04 +0200

    Base Structure building in win

--- PartTimeSpaceHero/Classes/MapObject.hpp
+++ PartTimeSpaceHero/Classes/MapObject.hpp
@@ -16,9 +16,9 @@ class MapObject : public cocos2d::Node
 {
 public:
   // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-  MapObject();
-  void loadMap(const char* name);
+  virtual bool init();
   // implement the "static create()" method manually
+  CREATE_FUNC(MapObject);
   TMXTiledMap* map;
 private:
   
--- PartTimeSpaceHero/Classes/WorldObject.cpp
+++ PartTimeSpaceHero/Classes/WorldObject.cpp
@@ -7,16 +7,11 @@
 //
 
 #include "WorldObject.hpp"
-#include "MapObject.hpp"
 
-WorldObject::WorldObject() {
 
-}
-
-void WorldObject::setupWorld() {
-  MapObject* mapObject = new MapObject();
-  auto tmx = new TMXTiledMap();
-  FileUtils::getInstance()->addSearchPath("res");
-  tmx = TMXTiledMap::create("alpha.tmx");
+bool WorldObject::init() {
+  mapObject = MapObject::create();
   addChild(mapObject);
+  return true;
 }
+
